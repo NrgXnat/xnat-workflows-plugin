aad3dc25da828c1c5266bcd36319c07ba0525302
John Flavin <flavinj@gmail.com>
2018-07-13 13:53:07 -0500

    Modify plugin config so it works

--- src/main/java/org/nrg/xnat/plugins/workflows/config/WorkflowsPluginConfig.java
+++ src/main/java/org/nrg/xnat/plugins/workflows/config/WorkflowsPluginConfig.java
@@ -1,16 +1,19 @@
 package org.nrg.xnat.plugins.workflows.config;
 
 import org.flowable.engine.ProcessEngine;
-import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.flowable.spring.ProcessEngineFactoryBean;
 import org.flowable.spring.SpringProcessEngineConfiguration;
 import org.nrg.framework.annotations.XnatPlugin;
 import org.nrg.xnat.initialization.RootConfig;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -20,36 +23,35 @@ import javax.sql.DataSource;
         description = "XNAT Workflows Plugin",
         entityPackages = "org.nrg.xnat.plugins.workflows",
         log4jPropertiesFile = "META-INF/resources/log4j.properties",
-        version = ""
+        version = "1.0.0-SNAPSHOT"
 )
 @ComponentScan(value = "org.nrg.xnat.plugins.workflows",
         excludeFilters = @Filter(type = FilterType.REGEX, pattern = ".*TestConfig.*", value = {}))
 @Import({RootConfig.class})
 public class WorkflowsPluginConfig {
-    // @Bean
-    // public Module guavaModule() {
-    //     return new GuavaModule();
-    // }
-    //
-    // @Bean
-    // public ObjectMapper objectMapper(final Jackson2ObjectMapperBuilder objectMapperBuilder) {
-    //     return objectMapperBuilder.build();
-    // }
+    @Bean
+    public ProcessEngineConfigurationImpl processEngineConfiguration(final DataSource dataSource,
+                                                                     final PlatformTransactionManager transactionManager) {
+        final SpringProcessEngineConfiguration processEngineConfiguration = new SpringProcessEngineConfiguration();
+        processEngineConfiguration.setTransactionManager(transactionManager);
+        processEngineConfiguration.setDataSource(dataSource);
+        processEngineConfiguration.setDatabaseSchemaUpdate("true");
 
-    // @Bean
-    // public TriggerTask dockerEventPullerTask(final DockerStatusUpdater dockerStatusUpdater) {
-    //     return new TriggerTask(
-    //             dockerStatusUpdater,
-    //             new PeriodicTrigger(10L, TimeUnit.SECONDS)
-    //     );
-    // }
+        return processEngineConfiguration;
+    }
 
     @Bean
-    public ProcessEngine processEngine(final DataSource dataSource) {
-        final ProcessEngineConfiguration processEngineConfiguration = new SpringProcessEngineConfiguration()
-                .setDataSource(dataSource)
-                .setDatabaseSchemaUpdate("true");
+    public ProcessEngineFactoryBean processEngineFactoryBean(final ProcessEngineConfigurationImpl processEngineConfiguration,
+                                                             final ApplicationContext ctx) {
+        final ProcessEngineFactoryBean processEngineFactoryBean = new ProcessEngineFactoryBean();
+        processEngineFactoryBean.setProcessEngineConfiguration(processEngineConfiguration);
+        processEngineFactoryBean.setApplicationContext(ctx);
 
-        return processEngineConfiguration.buildProcessEngine();
+        return processEngineFactoryBean;
+    }
+
+    @Bean
+    public ProcessEngine processEngine(final ProcessEngineFactoryBean processEngineFactoryBean) throws Exception {
+        return processEngineFactoryBean.getObject();
     }
 }
